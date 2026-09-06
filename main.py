03d7b1a91fddb6ac9422cfafa15b31db7996ee56
Muaz V <muzyyyyyyy@gmail.com>
2026-09-07 02:12:06 +0530

    Added formatting

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 import requests
-import json
+from rich.console import Console
+# import json
 
-print('--------------------------------')
-print('WEATHER')
-print('--------------------------------')
+console = Console()
+
+# print('--------------------------------')
+# print('WEATHER')
+console.rule("[bold #e24d00] WEATHER", style="#e24d00")
+# print('--------------------------------')
 with open("api_key.txt", "r") as file:
     api_key = file.read()
 
@@ -19,15 +23,19 @@ def fetch_data(loc):
         return f"Code {response.status_code} Error. Pls Try Again Later"
 
 def display_data(data):
-    print("-------------------------")
-    print(data["address"].upper())
-    print("-------------------------")
-    print("******************************************")
-    print(f'Temperature: {data["currentConditions"]["temp"]}°C '
-          f'\nPrecipitation Probability: {data["currentConditions"]["precipprob"]}% '
-          f'\nHumidity: {data["currentConditions"]["humidity"]}% '
-          f'\nCurrent Condition: {data["currentConditions"]["conditions"]} '
-          f'\n******************************************')
+    # print("-------------------------")
+    # print(data["address"].upper())
+    console.rule(f"[bold #e24d00] {data["address"].upper()}", style="#e24d00")
+    # print("-------------------------")
+    console.print(f"[bold #55ffff]» Temperature: {data["currentConditions"]["temp"]}°C")
+    console.print(f"[bold #55ffff]» Precipitation Probability: {data["currentConditions"]["precipprob"]}%")
+    console.print(f"[bold #55ffff]» Humidity: {data["currentConditions"]["humidity"]}%")
+    console.print(f"[bold #55ffff]» Current Condition: {data["currentConditions"]["conditions"]}")
+    # print(f'Temperature: {data["currentConditions"]["temp"]}°C '
+    #       f'\nPrecipitation Probability: {data["currentConditions"]["precipprob"]}% '
+    #       f'\nHumidity: {data["currentConditions"]["humidity"]}% '
+    #       f'\nCurrent Condition: {data["currentConditions"]["conditions"]} '
+    console.rule(f"[bold #e24d00]")
 
 while True:
     location = input("Enter City Name: ").strip().lower()
